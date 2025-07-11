598dfa207ed0c0765a4f0aec00a4b32aa17795de
James McMinn <james.mcminn@fullfact.org>
2025-07-11 13:06:18 +0100

    Revert "chore: construct dsn from env vars"
    This reverts commit b8a9e328e2e256d5c9f738e377e0cb997d446941.

--- core/app.py
+++ core/app.py
@@ -13,15 +13,9 @@ from core.videos.controller import VideoController
 
 
 MIGRATION_TARGET_VERSION = 1
-DB_HOST = os.environ.get("DATABASE_HOST", "localhost")
-DB_PORT = os.environ.get("DATABASE_PORT", "5433")
-DB_USER = os.environ.get("DATABASE_USER", "pas")
-DB_PASSWORD = os.environ.get("DATABASE_PASSWORD", "s3cret")
-DB_NAME = os.environ.get("DATABASE_NAME", "pas")
-dsn = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+DB_URI = os.environ.get("DATABASE_URI", "postgresql://pas:s3cret@localhost:5433/pas")
 pool = AsyncConnectionPool(
-    dsn,
+    DB_URI,
     open=False,
     max_size=10,
     connection_class=AsyncConnection[DictRow],
