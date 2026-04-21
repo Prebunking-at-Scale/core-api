c1317a683af8d71c80af9255860514c45a8fba8a
David Fernández <dfernandez@maldita.es>
2026-04-21 12:30:50 +0000

    chore: update migration number

--- core/migrations/18.add-evolution-description-to-narratives.up.sql
+++
@@ -1,5 +0,0 @@
-BEGIN;
-
-ALTER TABLE narratives ADD COLUMN evolution_description TEXT;
-
-COMMIT;
--- core/migrations/19.rename-evolution-description-to-narrative-context.up.sql
+++
@@ -1,2 +0,0 @@
--- Rename evolution_description column to narrative_context
-ALTER TABLE narratives RENAME COLUMN evolution_description TO narrative_context;
